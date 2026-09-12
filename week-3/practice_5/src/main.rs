46c75d877454eacf5eb9312225a16ff11b393a44
PAU <esther.akpan@pau.edu.ng>
2026-09-12 16:13:20 +0100

    Add Week 3 Rust practices

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+	let Special_character = '@'; 	//default
+	let Alphabet:char = 'A';
+	let surname = "Aniefiok";
+	let first_name = "Star";
+	let middle_name = "Joseph";
+	
+	println!("");
+	println!("Special character: {}", Special_character);
+	println!("Alphabet: {}",Alphabet);
+	println!("Name: {} {} {}",first_name, surname, middle_name);
+}
+
---
+++ week-3/practice_8/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+	let fees_value = 25_000;
+	println!("fees is {}",fees_value);
+	
+	let fees = 35_000;
+	println!("fees changed is {}",fees);
+}
