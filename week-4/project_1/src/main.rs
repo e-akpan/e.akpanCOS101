651b744a912abb4b456b2d3719c1347e2ecd6369
PAU <esther.akpan@pau.edu.ng>
2026-09-20 00:33:57 +0100

    Complete quadratic root calculations

--- week-4/project_1/src/main.rs
+++ week-4/project_1/src/main.rs
@@ -9,33 +9,44 @@
 
         println!("What is the value for a?");
         io::stdin().read_line(&mut a).expect("Failed to read a");
-        let a:i32 = a.trim().parse().expect("Failed to read a");
+        let a:f32 = a.trim().parse().expect("Failed to read a");
         println!("The value for a is {}",a);
 
         println!("What is the value for b?");
         io::stdin().read_line(&mut b).expect("Failed to read b");
-        let b:i32 = b.trim().parse().expect("Failed to read b");
+        let b:f32 = b.trim().parse().expect("Failed to read b");
         println!("The value for b is {}",b);
 
         println!("What is the value for c?");
         io::stdin().read_line(&mut c).expect("Failed to read c");
-        let c:i32 = c.trim().parse().expect("Failed to read c");
+        let c:f32 = c.trim().parse().expect("Failed to read c");
         println!("The value for c is {}",c);
 
 
         println!("Finding Discriminant");
-        let d:i32 = b*b - 4*a*c;
+        let d:f32 = b*b - 4.0*a*c;
         println!("Discriminant (d) = {}",d);
 
-        if d > 0 {
-            println!("two distinct roots");
+        if d > 0.0 {
+            let root = d.sqrt();
 
-        } else if d == 0 {
-            println!("exactly one real root");
+            let x1 = (-b + root) / (2.0 * a);
+            let x2 = (-b - root) / (2.0 * a);
 
-        } else if d < 0 {
+            println!(" There are two distinct roots");
+            println!("x1 = {}",x1);
+            println!("x2 = {}",x2);
+
+
+        } else if d == 0.0 {
+            let x = -b / (2.0 * a);
+            println!("There is exactly one real root");
+            println!("x = {}",x);
+
+
+        } else if d < 0.0 {
             println!("no real roots");
-            
+
         } else {
             println!("No record");
         }
